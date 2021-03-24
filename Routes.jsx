55c07e8f11ad552020da3a2bc85333fe06ae9dac
Jimmy Ko <ko.jimmy09@gmail.com>
2021-03-24 01:24:25 -0400

    rendered text input search bar to Search component

--- Routes.jsx
+++ Routes.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 import axios from 'axios'
 import {createStackNavigator} from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native'
-import {View, Text, Button} from 'react-native'
+import {View, Text, Button, TextInput, StyleSheet} from 'react-native'
 import {Center} from './Center.jsx'
 
 const Stack = createStackNavigator();
 
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 200,
+    margin: 15,
+    borderWidth: 1,
+  },
+});
+
 const Search = ({navigation}) => {
+  const [text, onChangeText] = React.useState('Search by keyword tag');
+
   return (
     <Center>
       <Text>Search Bar Goes Here</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
       <Button title='Find Images' onPress={() => {
         navigation.navigate('Results')
       }}></Button>
