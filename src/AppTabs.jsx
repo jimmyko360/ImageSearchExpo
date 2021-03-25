968563610888d50a897a86722f343ee0afb2223b
Jimmy Ko <ko.jimmy09@gmail.com>
2021-03-25 02:22:52 -0400

    rendered expor vector icons on tab navigator

--- src/AppTabs.jsx
+++ src/AppTabs.jsx
@@ -4,13 +4,38 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {Search} from './tabs/Search.jsx'
 import {Results} from './tabs/Results.jsx'
 import {Details} from './tabs/Details.jsx'
+import {Ionicons} from '@expo/vector-icons';
 
 const Tabs = createBottomTabNavigator();
 
 export const AppTabs = ({}) => {
   return (
     <NavigationContainer>
-      <Tabs.Navigator initialRouteName='Search'>
+      <Tabs.Navigator
+        initialRouteName='Search'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Search') {
+              iconName = focused
+                ? 'search-circle'
+                : 'search-circle-outline';
+            } else if (route.name === 'Results') {
+              iconName = focused ? 'md-albums' : 'md-albums-outline';
+            } else if (route.name === 'Details') {
+              iconName = focused ? 'list-circle' : 'list-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tabs.Screen name='Search' title='Search' component={Search}/>
         <Tabs.Screen name='Results' title='Results' component={Results}/>
         <Tabs.Screen name='Details' title='Details' component={Details}/>
