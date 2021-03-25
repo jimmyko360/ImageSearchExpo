f367c12e993199154eae5cffefd7a6aedfb6893a
Jimmy Ko <ko.jimmy09@gmail.com>
2021-03-25 12:43:21 -0400

    added instructions to set up your own connection to the Pixabay API

---
+++ example.config.js
@@ -0,0 +1,11 @@
+/*
+HOW TO CONNECT TO PIXABAY API:
+
+-Create a copy of this file in the repository's root directory and rename it 'config.js'
+-Replace the 'PIXABAY_API_KEY_GOES_HERE' placeholder with your Pixabay API key as a string
+
+*/
+
+module.exports = {
+  API_TOKEN: 'PIXABAY_API_KEY_GOES_HERE'
+}
